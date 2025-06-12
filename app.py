97d8d3d5722a67e78452272a50866a4d93d27918
Khudaybergan <106686449+Khudaybergan@users.noreply.github.com>
2025-06-12 17:30:55 +0500

    Update app.py
    Added port

--- app.py
+++ app.py
@@ -75,3 +75,8 @@ def get_result(task_id):
 
     result = results[task_id]
     return jsonify(result)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))  # Используй PORT, который задаёт Render
+    app.run(host='0.0.0.0', port=port)
